fced37207532e7f7502f89696adf0dbcf12e30f4
Evan Johnson <evan.johnson@rapp.com>
2015-04-28 16:14:46 -0500

    Text edit.

--- library/MockMaker/Worker/MockMakerClassWorker.php
+++ library/MockMaker/Worker/MockMakerClassWorker.php
@@ -3,7 +3,7 @@
 /**
  * 	MockMakerClassWorker
  *
- * 	@author		Evan Johnson <evan.johnson@rapp.com>
+ * 	@author		Evan Johnson
  * 	@created	Apr 28, 2015
  * 	@version	1.0
  */
--- tests/MockMaker/Worker/MockMakerClassWorkerTest.php
+++ tests/MockMaker/Worker/MockMakerClassWorkerTest.php
@@ -3,7 +3,7 @@
 /**
  * 	MockMakerClassWorkerTest
  *
- * 	@author		Evan Johnson <evan.johnson@rapp.com>
+ * 	@author		Evan Johnson
  * 	@created	Apr 28, 2015
  * 	@version	1.0
  */
