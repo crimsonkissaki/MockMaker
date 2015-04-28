c3d389c187ea9def4ccd0fcf08a6345b1251c73c
Evan Johnson <evan.johnson@rapp.com>
2015-04-28 10:03:55 -0500

    Edits to simpleentity for testing

--- tests/MockMaker/Entities/SimpleEntity.php
+++ tests/MockMaker/Entities/SimpleEntity.php
@@ -1,39 +1,41 @@
 <?php
 
 /**
- *	SimpleEntity
+ * 	SimpleEntity
  *
- *	A very simple entity used for testing.
+ * 	A very simple entity used for testing.
  *
- *	@author		Evan Johnson <evan.johnson@rapp.com>
- *	@created	Apr 19, 2015
- *	@version	1.0
+ * 	@author		Evan Johnson
+ * 	@created	Apr 19, 2015
+ * 	@version	1.0
  */
 
-namespace Minion\MockMakerBundle\Tests\Resources\Entities;
+namespace MockMaker\Entities;
+
+// these aren't actually used, they're just here for testing
+use MockMaker\Entities\TestEntity;
+use MockMaker\Entities\PropertyWorkerEntity;
 
 class SimpleEntity
 {
 
-	public $publicProperty;
+    public $publicProperty;
+    private $privateProperty;
+    protected $protectedProperty;
 
-	private $privateProperty;
+    public function publicFunction()
+    {
+        return true;
+    }
 
-	protected $protectedProperty;
+    private function privateFunction()
+    {
+        return true;
+    }
 
-	public function publicFunction()
-	{
-		return true;
-	}
-
-	private function privateFunction()
-	{
-		return true;
-	}
-
-	protected function protectedFunction()
-	{
-		return true;
-	}
+    protected function protectedFunction()
+    {
+        return true;
+    }
 
 }
