f4a4bf06d61a68741875d8839067d1c8376c2a3d
Evan Johnson <evan.johnson@rapp.com>
2015-04-28 16:05:45 -0500

    Directory path fixing methods.

--- library/MockMaker/Worker/StringFormatterWorker.php
+++ library/MockMaker/Worker/StringFormatterWorker.php
@@ -16,13 +16,8 @@ class StringFormatterWorker
 {
 
     /**
-     * Interpolate an associative array
-     * of values into a string.
-     * Placeholders in string must == $k of array.
-     *
-     * Please beware of keys that are similar, but different.
-     * E.g. 'destinationCity' and 'destinationCityId' will both
-     * be replaced by 'destinationCity' because of string matching.
+     * Interpolate an associative array of values into a string.
+     * Placeholders in string must === the array keys.
      *
      * @param	string	$str
      * @param	array	$vars
@@ -37,13 +32,49 @@ class StringFormatterWorker
         if (count($vars) > 0) {
             foreach ($vars as $k => $v) {
                 if (!is_array($v)) {
+                    $needle = "{$char}{$k}{$char}";
                     $txt = (is_null($v)) ? 'NULL' : $v;
-                    $str = str_replace($char . $k . $char, "{$txt}", $str);
+                    $str = str_replace($needle, $txt, $str);
                 }
             }
         }
 
         return $str;
+    }
+
+    /**
+     * Format (single|array of) directory paths.
+     *
+     * @param   $paths  array
+     * @return  mixed
+     */
+    public static function formatDirectoryPaths($paths)
+    {
+        if (is_array($paths)) {
+            $fixed = [ ];
+            foreach ($paths as $key => $path) {
+                $fixed[$key] = self::formatDirectoryPath($path);
+            }
+            return $fixed;
+        } else {
+            return self::formatDirectoryPath($paths);
+        }
+    }
+
+    /**
+     * Format a directory path.
+     *
+     * @param   $path   string
+     * @return  string
+     */
+    public static function formatDirectoryPath($path)
+    {
+        $fixedPath = trim($path);
+        if (substr($fixedPath, -1) !== '/') {
+            $fixedPath .= '/';
+        }
+
+        return $fixedPath;
     }
 
 }
---
+++ tests/MockMaker/Worker/StringFormatterWorkerTest.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * 	StringFormatterWorkerTest
+ *
+ * 	@author		Evan Johnson <evan.johnson@rapp.com>
+ * 	@created	Apr 28, 2015
+ * 	@version	1.0
+ */
+
+namespace MockMaker\Worker;
+
+use MockMaker\Worker\StringFormatterWorker as Formatter;
+
+class StringFormatterWorkerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_vsprint2_properlyFormatsStringWithSingleArg()
+    {
+        $string = "This is a test string: %value%";
+        $args = array(
+            'value' => '_testValue_',
+        );
+        $expected = "This is a test string: _testValue_";
+        $actual = Formatter::vsprintf2($string, $args);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_vsprint2_properlyFormatsStringWithSingleArg_withDifferentDelimiter()
+    {
+        $string = "This is a test string: *value*";
+        $args = array(
+            'value' => '_testValue_',
+        );
+        $expected = "This is a test string: _testValue_";
+        $actual = Formatter::vsprintf2($string, $args, '*');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_vsprint2_properlyFormatsStringWithMultipleArgs()
+    {
+        $string = "Test string with multiple values: %foo% %bar% %baz%";
+        $args = array(
+            'foo' => '_fooValue_',
+            'bar' => '_barValue_',
+            'baz' => '_bazValue_',
+        );
+        $expected = "Test string with multiple values: _fooValue_ _barValue_ _bazValue_";
+        $actual = Formatter::vsprintf2($string, $args);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_vsprint2_properlyFormatsStringWithMultipleArgsWithSimilarNames()
+    {
+        $string = "Test string with multiple values: %foo% %fooBar% %bar% %barBaz% %baz%";
+        $args = array(
+            'foo' => '_fooValue_',
+            'fooBar' => '_fooBarValue_',
+            'bar' => '_barValue_',
+            'barBaz' => '_barBazValue_',
+            'baz' => '_bazValue_',
+        );
+        $expected = "Test string with multiple values: _fooValue_ _fooBarValue_ _barValue_ _barBazValue_ _bazValue_";
+        $actual = Formatter::vsprintf2($string, $args);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_formatDirectoryPaths_returnsArrayOfTrimmedPathsWithTrailingSlashes()
+    {
+        $dirs = array(
+            '  ' . dirname(__FILE__) . '   ',
+            dirname(dirname(__FILE__)) . '/ ',
+            dirname(dirname(dirname(__FILE__))) . '                     ',
+        );
+        $expected = array(
+            dirname(__FILE__) . '/',
+            dirname(dirname(__FILE__)) . '/',
+            dirname(dirname(dirname(__FILE__))) . '/',
+        );
+        $actual = Formatter::formatDirectoryPaths($dirs);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_formatDirectoryPaths_returnsAssociativeArrayOfTrimmedPathsWithTrailingSlashes()
+    {
+        $dirs = array(
+            'dir1' => '  ' . dirname(__FILE__) . '   ',
+            'dir2' => dirname(dirname(__FILE__)) . '/ ',
+            'dir3' => dirname(dirname(dirname(__FILE__))) . '                     ',
+        );
+        $expected = array(
+            'dir1' => dirname(__FILE__) . '/',
+            'dir2' => dirname(dirname(__FILE__)) . '/',
+            'dir3' => dirname(dirname(dirname(__FILE__))) . '/',
+        );
+        $actual = Formatter::formatDirectoryPaths($dirs);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function formatDirectoryPathProvider()
+    {
+        return array(
+            array( '  ' . dirname(__FILE__) . '   ', dirname(__FILE__) . '/' ),
+            array( dirname(dirname(__FILE__)) . '/ ', dirname(dirname(__FILE__)) . '/' ),
+            array( dirname(dirname(dirname(__FILE__))) . '                     ', dirname(dirname(dirname(__FILE__))) . '/' ),
+        );
+    }
+
+    /**
+     * @dataProvider formatDirectoryPathProvider
+     */
+    public function test_formatDirectoryPaths_returnsSingleTrimmedPathWithTrailingSlash($dir, $expected)
+    {
+        $actual = Formatter::formatDirectoryPaths($dir);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @dataProvider formatDirectoryPathProvider
+     */
+    public function test_formatDirectoryPath_returnsTrimmedPathWithTrailingSlash($dir, $expected)
+    {
+        $actual = Formatter::formatDirectoryPath($dir);
+        $this->assertEquals($expected, $actual);
+    }
+
+}
