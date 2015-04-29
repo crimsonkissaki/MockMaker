bc6b7feb00a134b93f4c9e782085ec45345b3930
Evan Johnson <evan.johnson@rapp.com>
2015-04-29 01:56:07 -0500

    Adding more test data to TestEntity.

--- tests/MockMaker/Entities/TestEntity.php
+++ tests/MockMaker/Entities/TestEntity.php
@@ -12,41 +12,47 @@
 
 namespace MockMaker\Entities;
 
+use MockMaker\Entities\SimpleEntity;
+use stdClass;
+
 class TestEntity
 {
 
+    // const can only be public
     const CONSTANT_PROPERTY1 = 'constProperty1 value';
     const CONSTANT_PROPERTY2 = 'constProperty2 value';
 
-    //const CONSTANT_PROPERTY3 = 'constProperty3 value';
-    //const CONSTANT_PROPERTY4 = 'constProperty4 value';
     // public bicycle
     public $publicProperty1 = 'publicProperty1 value';
     public $publicProperty2 = 'publicProperty2 value';
-    public $publicProperty3 = 'publicProperty3 value';
-    public $publicProperty4 = 'publicProperty4 value';
+    public $publicPropertyDefaultValueAssignedByConstructor;
+    public $publicPropertyDefaultValueAssignedByConstructorTopLevelClass;
+    public static $publicStaticProperty1 = 'publicStaticProperty1 value';
+    public static $publicStaticProperty2 = 'publicStaticProperty2 value';
     // only this class
     private $privateProperty1 = 'privateProperty1 value';
     private $privateProperty2 = 'privateProperty2 value';
-    private $privateProperty3 = 'privateProperty3 value';
-    private $privateProperty4 = 'privateProperty4 value';
-    // this class and it's children
+    private $privatePropertyDefaultValueAssignedByConstructor;
+    private $privatePropertyDefaultValueAssignedByConstructorTopLevelClass;
+    private static $privateStaticProperty1 = 'privateStaticProperty1 value';
+    private static $privateStaticProperty2 = 'privateStaticProperty2 value';
+    // this class and it children
     protected $protectedProperty1 = 'protectedProperty1 value';
     protected $protectedProperty2 = 'protectedProperty2 value';
-    protected $protectedProperty3 = 'protectedProperty3 value';
-    protected $protectedProperty4 = 'protectedProperty4 value';
-    public static $publicStaticProperty1;
-    public static $publicStaticProperty2;
-    public static $publicStaticProperty3;
-    public static $publicStaticProperty4;
-    private static $privateStaticProperty1;
-    private static $privateStaticProperty2;
-    private static $privateStaticProperty3;
-    private static $privateStaticProperty4;
-    protected static $protectedStaticProperty1;
-    protected static $protectedStaticProperty2;
-    protected static $protectedStaticProperty3;
-    protected static $protectedStaticProperty4;
+    protected $protectedPropertyDefaultValueAssignedByConstructor;
+    protected $protectedPropertyDefaultValueAssignedByConstructorTopLevelClass;
+    protected static $protectedStaticProperty1 = 'protectedStaticProperty1 value';
+    protected static $protectedStaticProperty2 = 'protectedStaticProperty2 value';
+
+    public function __construct()
+    {
+        $this->publicPropertyDefaultValueAssignedByConstructor = new SimpleEntity();
+        $this->publicPropertyDefaultValueAssignedByConstructorTopLevelClass = new \DateTime();
+        $this->privatePropertyDefaultValueAssignedByConstructor = new SimpleEntity();
+        $this->privatePropertyDefaultValueAssignedByConstructorTopLevelClass = new \DateTime();
+        $this->protectedPropertyDefaultValueAssignedByConstructor = new SimpleEntity();
+        $this->protectedPropertyDefaultValueAssignedByConstructorTopLevelClass = new stdClass();
+    }
 
     public function getPublicProperty1()
     {
@@ -66,6 +72,11 @@ class TestEntity
     public function setPublicProperty2($publicProperty2)
     {
         $this->publicProperty2 = $publicProperty2;
+    }
+
+    public function setPublicTypehintedProperty(SimpleEntity $simpleEntity)
+    {
+        $this->publicTypehintedProperty = $simpleEntity;
     }
 
     public function getProtectedProperty1()
@@ -96,6 +107,16 @@ class TestEntity
     public function getPrivateProperty2()
     {
         return $this->protectedProperty2;
+    }
+
+    public function thisMethodIsNotARealSetter()
+    {
+        return __METHOD__;
+    }
+
+    public function gettingUpsetIfThisQualifiesAsASetter()
+    {
+        return __METHOD__;
     }
 
 }
