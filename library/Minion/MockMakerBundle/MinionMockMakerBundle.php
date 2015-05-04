82b9f94f40f81da21ad540d73497902ae453634c
Evan Johnson <evan.johnson@rapp.com>
2015-05-04 14:54:31 -0500

    Removed old repo directory.

--- library/Minion/MockMakerBundle/MinionMockMakerBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Minion\MockMakerBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MinionMockMakerBundle extends Bundle
-{
-}
--- library/Minion/UnitTestBundle/MinionUnitTestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Minion\UnitTestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MinionUnitTestBundle extends Bundle
-{
-}
